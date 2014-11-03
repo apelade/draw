363cadc168e4874b1a1536c9ae56516fc3c8a1a3
Paul McCulloch <hrfeels@gmail.com>
2014-11-03 13:43:47 -0800

    change getSpots to get 3 rows of 3 evenly, afflicting others with overlap

--- Algo.java
+++ Algo.java
@@ -9,94 +9,96 @@ import java.util.Set;
  */
 public class Algo {
 
-    /**
-     * In-place, snap a point to a grid.
-     * <p>
-     * @param toSnap Point which will be altered
-     * @param scalex Horizontal scale of grid to snap onto.
-     * @param scaley Vertical scale of grid to snap onto.
-     * @return toSnap Point with location changed.
-     */
-    public static Point snapPointInPlace(Point toSnap, int scalex, int scaley) {
-        toSnap.setLocation((toSnap.x / scalex) * scalex, (toSnap.y / scaley) * scaley);
-        return toSnap;
-    }
+	/**
+	 * In-place, snap a point to a grid.
+	 * <p>
+	 * 
+	 * @param toSnap
+	 *            Point which will be altered
+	 * @param scalex
+	 *            Horizontal scale of grid to snap onto.
+	 * @param scaley
+	 *            Vertical scale of grid to snap onto.
+	 * @return toSnap Point with location changed.
+	 */
+	public static Point snapPointInPlace(Point toSnap, int scalex, int scaley) {
+		toSnap.setLocation((toSnap.x / scalex) * scalex, (toSnap.y / scaley)
+				* scaley);
+		return toSnap;
+	}
 
-    /**
-     * Generate the points which define the top-left of squares
-     * symmetrically connecting a start and stop point.
-     * <p>
-     * Symmetry is complete when there is an even number of squares.
-     * Asymmetry should not be more than one square:
-     * <pre>
-     * ###       =>       ####        =>       ####
-     *    ###                 ###                  ####
-     * 
-     * 
-     * NOT:   ######
-     *              ##
-     * </pre>             
-     * Distribution of longer and shorter blocks is symmetrical:
-     * <pre>
-     * 
-     * #
-     * #
-     *  #         
-     *  #       0   line  (10, 100) to (100, 50)
-     *  #     00
-     *  #   00  
-     *   #00 
-     *  0X   X - intersection of # and 0
-     * 0 # 
-     *   #
-     *    # 
-     *    # 
-     *    # 
-     *    # 
-     *     # 
-     *     #
-     *     #   line (10,20) to (60,220):
-     * </pre>               
-     *               
-     * @param start  Point in pixel coordinates
-     * @param stop   Point in pixel coordinates
-     * @param width  grid horizontal scale in pixels
-     * @param height grid vertical scale in pixels
-     * @param snap   snap point to nearest grid defined by width and height
-     * @return       connecting points between start and stop
-     *
-     * @TODO currently (10, 10) to (90, 30) does not give 3 X 3
-     */
-    public static Set<Point> getSpots(Point start, Point stop, int width, int height, boolean snap) {
-        if(snap) {
-            start = Algo.snapPointInPlace(start, width, height);
-            stop = Algo.snapPointInPlace(stop,  width, height);
-        }
-        double xch = stop.x - start.x;
-        double ych = stop.y - start.y;
-        double numSpots = Math.max(Math.abs(xch / width), Math.abs(ych / height));
-        Set<Point> spots = new LinkedHashSet<Point>(512);
+	/**
+	 * Generate the points which define the top-left of squares symmetrically
+	 * connecting a start and stop point.
+	 * <p>
+	 * 
+	 * <pre>
+	 * Asymmetry is not more than one square:
+	 * 
+	 * ###       =>       ####        =>       ####          NOT!:   ######
+	 *    ###                 ###                  ####                    ##
+	 * 
+	 *               
+	 * Distribution of longer and shorter blocks can be symmetrical:     
+	 *        #
+	 *      ##
+	 *    ## 
+	 *  ## 
+	 * #
+	 * 
+	 * However calculations are from one end and don't ensure symmetry.
+	 * Currently (10, 10) to (90, 30) does not give 3 X 3 but instead:
+	 * ##  
+	 *   ####
+	 *       ###
+	 * </pre>
+	 * 
+	 * 
+	 * 
+	 * @param start
+	 *            Point in pixel coordinates
+	 * @param stop
+	 *            Point in pixel coordinates
+	 * @param width
+	 *            grid horizontal scale in pixels
+	 * @param height
+	 *            grid vertical scale in pixels
+	 * @param snap
+	 *            snap point to nearest grid defined by width and height
+	 * @return connecting points between start and stop
+	 * 
+	 */
+	public static Set<Point> getSpots(Point start, Point stop, int width,
+			int height, boolean snap) {
+		if (snap) {
+			start = Algo.snapPointInPlace(start, width, height);
+			stop = Algo.snapPointInPlace(stop, width, height);
+		}
+		double xch = stop.x - start.x;
+		double ych = stop.y - start.y;
+		double numSpots = Math.max(Math.abs(xch / width),
+				Math.abs(ych / height));
+		if(numSpots % 2 == 0) {
+			numSpots +=1;
+		}
+		Set<Point> spots = new LinkedHashSet<Point>(512);
+		double lenx = (xch / numSpots);
+		double leny = (ych / numSpots);
+		double x = start.x + .5 * width;
+		double y = start.y + .5 * height;
 
-        if (numSpots == 0) {
-            spots.add(start);
-        } else {
-            double lenx = (xch / numSpots);
-            double leny = (ych / numSpots);
-            double x = start.x + .5 * width;
-            double y = start.y + .5 * height;
-            Point toAdd;
-            
-            for (int i = 0; i < numSpots+1; i++) {
-                toAdd = new Point((int)x, (int)y);
-                if (snap == true) {
-                    spots.add(snapPointInPlace(toAdd, width, height));
-                }else {
-                    spots.add(toAdd);
-                }
-                x = x + lenx;
-                y = y + leny;
-            }
-        }
-        return spots;
-    }
+		for (int i = 0; i < numSpots + 1; i++) {
+		//	Point toAdd = new Point((int) Math.round(x), (int) Math.round(y));
+			Point toAdd = new Point((int) x, (int) y);
+
+			if (snap == true) {
+				spots.add(snapPointInPlace(toAdd, width, height));
+			} else {
+				spots.add(toAdd);
+			}
+			x = x + lenx;
+			y = y + leny;
+		}
+		return spots;
+	}
 }
--- AlgoTest.java
+++ AlgoTest.java
@@ -32,7 +32,7 @@ public class AlgoTest {
         		scalex,
         		scaley,
                 true);
-//        System.out.println("Outputs: " + Arrays.toString(pts.toArray()));
+        System.out.println("Outputs: " + Arrays.toString(pts.toArray()));
         assertTrue(testGetSpotsOutputDivisibleByScale(pts));
         return pts;
     }
@@ -40,6 +40,8 @@ public class AlgoTest {
      * Show that the layout of generated spots is correct
      */
     public void testGetSpots() {
+    	
+    	Set<Point> pts;
 
         /**
          *          #
@@ -50,22 +52,37 @@ public class AlgoTest {
          * #
          * 
          */
-        Set<Point> pts = getPointsForArray(new Point(10, 100), new Point(100, 50));
+        pts = getPointsForArray(new Point(10, 100), new Point(100, 50));
         assertEquals(
         	Arrays.toString(pts.toArray()),
-            "[java.awt.Point[x=10,y=100], java.awt.Point[x=20,y=90], java.awt.Point[x=30,y=90], java.awt.Point[x=40,y=80], java.awt.Point[x=50,y=80], java.awt.Point[x=60,y=70], java.awt.Point[x=70,y=70], java.awt.Point[x=80,y=60], java.awt.Point[x=90,y=60], java.awt.Point[x=100,y=50]]"
+        	"[java.awt.Point[x=10,y=100]," +
+        	" java.awt.Point[x=20,y=90], java.awt.Point[x=30,y=90]," +
+        	" java.awt.Point[x=40,y=80], java.awt.Point[x=50,y=80]," +
+        	" java.awt.Point[x=60,y=70], java.awt.Point[x=70,y=70]," +
+        	" java.awt.Point[x=80,y=60], java.awt.Point[x=90,y=60]," +
+        	" java.awt.Point[x=100,y=50]]"
+//            "[java.awt.Point[x=10,y=100], java.awt.Point[x=20,y=90], java.awt.Point[x=30,y=90], java.awt.Point[x=40,y=80], java.awt.Point[x=50,y=80], java.awt.Point[x=60,y=70], java.awt.Point[x=70,y=70], java.awt.Point[x=80,y=60], java.awt.Point[x=90,y=60], java.awt.Point[x=100,y=50]]"
+        	
+//        	"[java.awt.Point[x=10,y=100], java.awt.Point[x=20,y=100]," +
+//        	" java.awt.Point[x=30,y=90], java.awt.Point[x=40,y=90]," +
+//        	" java.awt.Point[x=50,y=80], java.awt.Point[x=60,y=80]," +
+//        	                             " java.awt.Point[x=60,y=70], java.awt.Point[x=70,y=70]," +
+//        	" java.awt.Point[x=80,y=60], java.awt.Point[x=90,y=60]," +
+//        	" java.awt.Point[x=100,y=50]]"
+
         );
-        
+//        
         /**
          * 
-         * ### 
+         * #### 
          *    ####
          * 
          */
-        pts = getPointsForArray(new Point(10, 10), new Point(70, 20));
+         pts = getPointsForArray(new Point(10, 10), new Point(70, 20));
         assertEquals(
         	Arrays.toString(pts.toArray()),
-            "[java.awt.Point[x=10,y=10], java.awt.Point[x=20,y=10], java.awt.Point[x=30,y=10], java.awt.Point[x=40,y=20], java.awt.Point[x=50,y=20], java.awt.Point[x=60,y=20], java.awt.Point[x=70,y=20]]"
+            "[java.awt.Point[x=10,y=10], java.awt.Point[x=20,y=10], java.awt.Point[x=30,y=10], java.awt.Point[x=40,y=10]," +
+            " java.awt.Point[x=40,y=20], java.awt.Point[x=50,y=20], java.awt.Point[x=60,y=20], java.awt.Point[x=70,y=20]]"
         );
         
         /**
@@ -77,33 +94,36 @@ public class AlgoTest {
         pts = getPointsForArray(new Point(10, 10), new Point(80, 20));
         assertEquals(
         	Arrays.toString(pts.toArray()),
-            "[java.awt.Point[x=10,y=10], java.awt.Point[x=20,y=10], java.awt.Point[x=30,y=10], java.awt.Point[x=40,y=10], java.awt.Point[x=50,y=20], java.awt.Point[x=60,y=20], java.awt.Point[x=70,y=20], java.awt.Point[x=80,y=20]]"
+            "[java.awt.Point[x=10,y=10], java.awt.Point[x=20,y=10], java.awt.Point[x=30,y=10], java.awt.Point[x=40,y=10]," +
+            " java.awt.Point[x=50,y=20], java.awt.Point[x=60,y=20], java.awt.Point[x=70,y=20], java.awt.Point[x=80,y=20]]"
         );
         
         /**
          * 
-         * ####  
+         * #####  
          *     #####
          * 
          */
         pts = getPointsForArray(new Point(10, 10), new Point(90, 20));
         assertEquals(
         	Arrays.toString(pts.toArray()),
-            "[java.awt.Point[x=10,y=10], java.awt.Point[x=20,y=10], java.awt.Point[x=30,y=10], java.awt.Point[x=40,y=10], java.awt.Point[x=50,y=20], java.awt.Point[x=60,y=20], java.awt.Point[x=70,y=20], java.awt.Point[x=80,y=20], java.awt.Point[x=90,y=20]]"
+            "[java.awt.Point[x=10,y=10], java.awt.Point[x=20,y=10], java.awt.Point[x=30,y=10], java.awt.Point[x=40,y=10], java.awt.Point[x=50,y=10]," +
+            " java.awt.Point[x=50,y=20], java.awt.Point[x=60,y=20], java.awt.Point[x=70,y=20], java.awt.Point[x=80,y=20], java.awt.Point[x=90,y=20]]"
         );
         
         /**
-         * @TODO: this should be 3 x 3 ideally
          * 
-         * ##  
-         *   ####
+         * ###  
+         *    ###
          *       ###
-         * 
+         *       
          */
         pts = getPointsForArray(new Point(10, 10), new Point(90, 30));
         assertEquals(
         	Arrays.toString(pts.toArray()),
-            "[java.awt.Point[x=10,y=10], java.awt.Point[x=20,y=10], java.awt.Point[x=30,y=20], java.awt.Point[x=40,y=20], java.awt.Point[x=50,y=20], java.awt.Point[x=60,y=20], java.awt.Point[x=70,y=30], java.awt.Point[x=80,y=30], java.awt.Point[x=90,y=30]]"
+            "[java.awt.Point[x=10,y=10], java.awt.Point[x=20,y=10], java.awt.Point[x=30,y=10]," +
+            " java.awt.Point[x=40,y=20], java.awt.Point[x=50,y=20], java.awt.Point[x=60,y=20]," +
+            " java.awt.Point[x=70,y=30], java.awt.Point[x=80,y=30], java.awt.Point[x=90,y=30]]"
         );
         
         /**
@@ -116,21 +136,45 @@ public class AlgoTest {
         pts = getPointsForArray(new Point(0, 10), new Point(90, 30));
         assertEquals(
         	Arrays.toString(pts.toArray()),
-            "[java.awt.Point[x=0,y=10], java.awt.Point[x=10,y=10], java.awt.Point[x=20,y=10], java.awt.Point[x=30,y=20], java.awt.Point[x=40,y=20], java.awt.Point[x=50,y=20], java.awt.Point[x=60,y=20], java.awt.Point[x=70,y=30], java.awt.Point[x=80,y=30], java.awt.Point[x=90,y=30]]"
+            "[java.awt.Point[x=0,y=10], java.awt.Point[x=10,y=10], java.awt.Point[x=20,y=10]," +
+            " java.awt.Point[x=30,y=20], java.awt.Point[x=40,y=20], java.awt.Point[x=50,y=20], java.awt.Point[x=60,y=20]," +
+            " java.awt.Point[x=70,y=30], java.awt.Point[x=80,y=30], java.awt.Point[x=90,y=30]]"
         );
         
+//    	/**
+//    	*                   ##
+//    	*               ####
+//    	*           ####
+//    	*       ####  
+//    	*   ####   
+//    	* ##
+//    	*/                
+//        pts = getPointsForArray(new Point(20, 60), new Point(220, 10));
+//        assertEquals(
+//        	Arrays.toString(pts.toArray()),
+//            "[java.awt.Point[x=20,y=60], java.awt.Point[x=30,y=60], java.awt.Point[x=40,y=60], java.awt.Point[x=50,y=50], java.awt.Point[x=60,y=50], java.awt.Point[x=70,y=50], java.awt.Point[x=80,y=50], java.awt.Point[x=90,y=40], java.awt.Point[x=100,y=40], java.awt.Point[x=110,y=40], java.awt.Point[x=120,y=40], java.awt.Point[x=130,y=30], java.awt.Point[x=140,y=30], java.awt.Point[x=150,y=30], java.awt.Point[x=160,y=30], java.awt.Point[x=170,y=20], java.awt.Point[x=180,y=20], java.awt.Point[x=190,y=20], java.awt.Point[x=200,y=20], java.awt.Point[x=210,y=10], java.awt.Point[x=220,y=10]]"
+//        );
+        
     	/**
-    	*                   ##
+    	*                   ###
     	*               ####
     	*           ####
-    	*       ####  
-    	*   ####   
-    	* ##
+    	*        ####  
+    	*    ####   
+    	* ###
     	*/                
         pts = getPointsForArray(new Point(20, 60), new Point(220, 10));
         assertEquals(
         	Arrays.toString(pts.toArray()),
-            "[java.awt.Point[x=20,y=60], java.awt.Point[x=30,y=60], java.awt.Point[x=40,y=60], java.awt.Point[x=50,y=50], java.awt.Point[x=60,y=50], java.awt.Point[x=70,y=50], java.awt.Point[x=80,y=50], java.awt.Point[x=90,y=40], java.awt.Point[x=100,y=40], java.awt.Point[x=110,y=40], java.awt.Point[x=120,y=40], java.awt.Point[x=130,y=30], java.awt.Point[x=140,y=30], java.awt.Point[x=150,y=30], java.awt.Point[x=160,y=30], java.awt.Point[x=170,y=20], java.awt.Point[x=180,y=20], java.awt.Point[x=190,y=20], java.awt.Point[x=200,y=20], java.awt.Point[x=210,y=10], java.awt.Point[x=220,y=10]]"
+        	
+           "[java.awt.Point[x=20,y=60], java.awt.Point[x=30,y=60], java.awt.Point[x=40,y=60]," +
+           " java.awt.Point[x=50,y=50], java.awt.Point[x=60,y=50], java.awt.Point[x=70,y=50], java.awt.Point[x=80,y=50]," +
+           " java.awt.Point[x=90,y=40], java.awt.Point[x=100,y=40], java.awt.Point[x=110,y=40], java.awt.Point[x=120,y=40]," +
+           " java.awt.Point[x=120,y=30], java.awt.Point[x=130,y=30], java.awt.Point[x=140,y=30], java.awt.Point[x=150,y=30]," +
+           " java.awt.Point[x=160,y=20], java.awt.Point[x=170,y=20], java.awt.Point[x=180,y=20], java.awt.Point[x=190,y=20]," +
+           " java.awt.Point[x=200,y=10], java.awt.Point[x=210,y=10], java.awt.Point[x=220,y=10]]"
+        	
+           // "[java.awt.Point[x=20,y=60], java.awt.Point[x=30,y=60], java.awt.Point[x=40,y=60], java.awt.Point[x=50,y=50], java.awt.Point[x=60,y=50], java.awt.Point[x=70,y=50], java.awt.Point[x=80,y=50], java.awt.Point[x=90,y=40], java.awt.Point[x=100,y=40], java.awt.Point[x=110,y=40], java.awt.Point[x=120,y=40], java.awt.Point[x=130,y=30], java.awt.Point[x=140,y=30], java.awt.Point[x=150,y=30], java.awt.Point[x=160,y=30], java.awt.Point[x=170,y=20], java.awt.Point[x=180,y=20], java.awt.Point[x=190,y=20], java.awt.Point[x=200,y=20], java.awt.Point[x=210,y=10], java.awt.Point[x=220,y=10]]"
         );
     }
     
